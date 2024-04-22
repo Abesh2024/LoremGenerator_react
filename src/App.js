80f8b63b7eb71dad805cb1586d481fb2266b5ea0
Abesh <abeshsarkar2030@gmail.com>
2024-04-23 02:52:57 +0530

    final update

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
-import Main from './Components/Main';
+import Lorem from './Components/Lorem';
 
 function App() {
   return (
     <div className="App">
-      <Main/>
+      <Lorem/>
     </div>
   );
 }
